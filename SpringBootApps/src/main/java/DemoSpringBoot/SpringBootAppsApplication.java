691195c5c844bf8a11e371bc6bfa27ae42a44d5a
akanjain <akanjain@inlen8520030366.corp.capgemini.com>
2022-04-13 15:10:52 +0530

    using mvc and rest in springBoot

---
+++ SpringBootApps/src/main/java/DemoSpringBoot/SpringBootAppsApplication.java
@@ -0,0 +1,14 @@
+package DemoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppsApplication.class, args);
+	}
+	
+
+}
